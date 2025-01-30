1b3f0bd0b8039a720cb0deafa7ba2b2f6c373bc3
Esteban Montoya <estebanmontya14@gmail.com>
2025-01-29 22:17:10 -0500

    actualizacion

--- controldecasos/controldecasos/settings.py
+++ controldecasos/controldecasos/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'controldecasos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_mysql',
+        'USER': 'root',
+        'PASSWORD': '3240160',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
